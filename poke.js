a6606d5a7465b82a22d5d8b7a085315d3adee334
thr3a <thr3a@example.com>
2020-01-04 10:58:10 +0900

    clean: 不要なファイル削除

--- poke.js
+++ poke.js
@@ -1,44 +1,4 @@
 const pokeTypes = require('poke-types')
 
-console.log(pokeTypes.getTypeWeaknesses('water', 'flying'))
-
+console.log(pokeTypes.getTypeWeaknesses('fairy', 'dragon'))
 console.log(pokeTypes.getTypeStrengths('electric'))
-
-// import { getTypeWeaknesses } from 'poke-types'
-// import { noEffect, notVeryEffective, superEffective, ultraEffective } from 'poke-types/effectiveness'
-
-// const abilityEffectiveness = (targetType, abilityType) => {
-//   switch (getTypeWeaknesses(...targetType)[abilityType]) {
-//     case noEffect: return 'It has no effect...'
-//     case notVeryEffective: return 'It\'s not very effective...'
-//     case superEffective: case ultraEffective: return 'It\'s super effective!'
-//     default: return ''
-//   }
-// }
-
-// const attack = pokemon => target => ability => {
-//   return `
-//   ${pokemon.name} used ${ability.name}!
-//             💥
-//   ${abilityEffectiveness(target.type, ability.type)}
-//   `
-// }
-
-// const pikachu = {
-//   name: 'Pikachu',
-//   type: 'electric',
-//   abilities: {
-//     thunderbolt: {
-//       name: 'Thunderbolt',
-//       type: 'electric'
-//     }
-//   },
-//   attack: target => ability => attack(pikachu)(target)(pikachu.abilities[ability])
-// }
-
-// const gyarados = {
-//   name: 'Gyarados',
-//   type: ['water', 'flying']
-// }
-
-// console.log(pikachu.attack(gyarados)('thunderbolt'))
