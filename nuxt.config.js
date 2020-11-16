841a422d6bd7dfecb5e81a793fcd332d4e43e47a
thr3a <a@example.com>
2020-11-17 00:49:01 +0900

    add: OGP対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,16 +5,28 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#'
+    },
+    title: 'ポケモンタイプ相性チェッカー',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'ポケモンタイプ相性を簡単にチェック' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'ポケモンタイプ相性チェッカー' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'http://poketype.turai.work/' },
+      { hid: 'og:title', property: 'og:title', content: 'ポケモンタイプ相性チェッカー' },
+      { hid: 'og:description', property: 'og:description', content: '共通ディスクリプション' },
+      { hid: 'og:image', property: 'og:image', content: '/ogp.jpg' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:image', content: '/ogp.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+    ],
+},
   /*
   ** Customize the progress-bar color
   */
